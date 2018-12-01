043fbd7c2a8af7d7beea2c8969095d3d2e485be5
richardtz12 <richardtz12@gmail.com>
2018-12-01 01:11:13 -0500

    Got rid of dummy data in show-hist and got exhibits to pull its select attrbitutes from the database

--- app.js
+++ app.js
@@ -229,6 +229,15 @@ app.get("/pull_all_shows", urlencodedParser,  function(req, res) {
     });
 });
 
+app.get("/pull_Exhibits", urlencodedParser,  function(req, res) {
+  con.query('SELECT Exhibit FROM Animal_Show', function(err,rows) {
+      if (err) throw err;
+      console.log('Data received from Db:\n');
+      console.log(rows);
+      res.json(rows)
+  });
+});
+
 app.get("/add_show", urlencodedParser,  function(req, res) {
     res.sendFile(path.join(__dirname,'./html/add-show.html'));
 });
