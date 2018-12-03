f7c2b725d7724d7a3e6556e90a61965573d64fa0
richardtz12 <richardtz12@gmail.com>
2018-12-02 20:50:16 -0500

    Logging showvisits doesn't work

--- js/mysql.js
+++ js/mysql.js
@@ -131,8 +131,9 @@ exports.logVisit = function(con, currentUser, currentExhibit, date, callback) {
 
 exports.logShow = function(con, currentUser, currentExhibit, currentDate, name, dateTime, callback) {
   console.log("attempting to log a visit and a show")
+  console.log(currentDate + dateTime);
   var log_query = "INSERT INTO Exhibit_Visits (Visitor, Name, DateTime) VALUES ('" + currentUser + "', '" + currentExhibit + "', '" + currentDate + "');"
-  log_query1 = "INSERT INTO Show_Visits (Name, DateTime, Visitor) VALUES ('" + name + "', '" +  + "', '" + dateTime + "', '" + currentUser + "');"
+  var log_query1 = "INSERT INTO Show_Visits (Name, DateTime, Visitor) VALUES ('" + name + "', '" +  + "', '" + dateTime + "', '" + currentUser + "');"
   console.log(log_query)
   console.log(log_query1)
 
@@ -143,6 +144,16 @@ exports.logShow = function(con, currentUser, currentExhibit, currentDate, name, 
       return callback(0);
     }
   });
+}
+
+exports.logShow1 = function(con, currentUser, currentExhibit, currentDate, name, dateTime, callback) {
+  console.log("attempting to log a visit and a show")
+  console.log(currentDate, dateTime);
+  var log_query = "INSERT INTO Exhibit_Visits (Visitor, Name, DateTime) VALUES ('" + currentUser + "', '" + currentExhibit + "', '" + currentDate + "');"
+  var log_query1 = "INSERT INTO Show_Visits (Name, DateTime, Visitor) VALUES ('" + name + "', '" +  + "', '" + dateTime + "', '" + currentUser + "');"
+  console.log(log_query)
+  console.log(log_query1)
+
   con.query(log_query1, function (err, result) {
     if (err){
       return callback(1);
